a4c118be617975acece0b3df8fc7428e1bc157a4
Ce-lby <ST10513486@rcconnect.edu.za>
2026-09-17 14:56:05 +0200

    i created a login class

---
+++ src/main/java/com/mycompany/prog/part1/LoginClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prog.part1;
+
+/**
+ *
+ * @author Celby
+ */
+public class LoginClass {
+    
+}
